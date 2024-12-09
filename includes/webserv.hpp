0fc6bf317e4d34b7dd4279ef14519f5b53511469
ArtuCN <arturo.conti.2004@gmail.com>
2024-12-09 18:01:27 +0100

    artu's branch created

---
+++ includes/webserv.hpp
@@ -0,0 +1,8 @@
+#ifndef WEBSERVER_HPP
+#define WEBSERVER_HPP
+
+#include <iostream>
+
+class 
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "includes/webserv.hpp"
+
+int main(int ac, char **av)
+{
+	(void)av;
+	if (ac != 2)
+		return (std::cout<<"ERROR: WRONG NUMBER OF ARGUMENTS\n", 0);
+	(void)av;
+}
