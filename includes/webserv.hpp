dc089b06815b8f5fb1736685cffa6b7f1747640b
ArtuCN <arturo.conti.2004@gmail.com>
2024-12-10 17:43:00 +0100

    added some classes

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -1,8 +1,83 @@
 #ifndef WEBSERVER_HPP
 #define WEBSERVER_HPP
 
+#include <cstdint>
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <map>
+#include <vector>
 
-class 
+// class mySocket
+// {
+// 	struct sockaddr_in _addr;
+
+// };
+
+class cgi
+{
+	private:
+		std::vector<int> fd;
+	public:
+		int excecute();
+		cgi();
+		~cgi();
+};
+
+
+class http
+{
+	private:
+		std::map<int, std::string> _error;
+		std::map<std::string, std::string> _data;
+		int _maxBodySize;
+
+	public:
+		http();
+		~http();
+};
+
+class location
+{
+	private:
+		std::map<std::string, std::string> _root;
+	public:
+		location();
+		~location();
+};
+
+class server
+{
+	private:
+		std::vector<int> _sockets;
+		std::map<std::string, std::string> _data;
+		std::vector<location> _locations;
+		bool _listing;
+	public:
+
+		int start();
+		int accept();
+		int stop();
+
+		server();
+		~server();
+};
+
+class conf
+{
+	private:
+		http _http;
+		std::vector<server> _servers;
+		cgi _cgi;
+	public:
+		int reload(int port);
+		conf();
+		~conf();
+};
+
+
+
+
 
 #endif
